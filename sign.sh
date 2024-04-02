4b9a3f1b60324080f4492d5725e24dac59538868
Steven Sherry <steven.r.sherry@gmail.com>
2024-04-02 14:26:41 -0500

    chore: Update sign-xcframework to take the secret as a parameter instead of baking it into the action itself. This will allow us to reuse the action across public and internal repos

--- sign.sh
+++ sign.sh
@@ -1,14 +1,19 @@
 set -eo pipefail
 
-echo $DEV_ID_BASE64 | base64 --decode -o dev_id.p12
+json=$(echo $1 | base64 --decode | jq -r)
+cert=$(echo $json | jq -r '.cert')
+password=$(echo $json | jq -r '.password')
+identity=$(echo $json | jq -r '.id')
+
+echo $cert | base64 --decode -o dev_id.p12
 
 security create-keychain -p temp temp.keychain
 security unlock -p temp temp.keychain
-security import dev_id.p12 -k temp.keychain -P $DEV_ID_PASSWORD -T /usr/bin/codesign
+security import dev_id.p12 -k temp.keychain -P $password -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k temp temp.keychain
 security list-keychains -d user -s temp.keychain
 
-codesign --timestamp -s "$DEV_ID_IDENTITY" $1
+codesign --timestamp -s "$identity" $2
 
 security delete-keychain temp.keychain
 rm dev_id.p12
